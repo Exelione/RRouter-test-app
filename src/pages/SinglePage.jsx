7519508e53a349ecf23d5d52cd64be7a49e0f171
Exelione <exelione@gmail.com>
2024-05-19 21:29:43 +0300

    to v6.4 / loaders

--- src/pages/SinglePage.jsx
+++ src/pages/SinglePage.jsx
@@ -1,38 +1,80 @@
-import { useEffect, useState } from "react"
-import { Link, useNavigate, useParams } from "react-router-dom"
+import { Await, Link, defer, useAsyncValue, useLoaderData, useNavigate } from "react-router-dom"
 import { CustomButton } from "../components/CustomButton";
+import { Suspense } from "react";
 
+const Post =  () => {
+    const post = useAsyncValue()
+    return (
+        <>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
 
-
+            <h5>{post.comments}</h5>
+        </>
+    )
+}
+const Comments = () => {
+    const comments = useAsyncValue();
+    return(
+        <div>
+            <h2>Comments</h2>
+            {comments.map(comment =>(
+                <>
+                <h3>{comment.email}</h3>
+                <h4>{comment.name}</h4>
+                <p>{comment.body}</p>
+                </>
+            ))}
+        </div>
+    )
+}
 
 const SinglePage = () => {
-    const { id } = useParams()
+
     const navigate = useNavigate();
-    const [post, setPost] = useState([]);
+    const { post, id, comments } = useLoaderData()
 
     const goBack = () => navigate(-1)
 
 
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-            .then(res => res.json())
-            .then(data => setPost(data))
-    }, [id])
-    return (
 
+    return (
         <div>
-            {post && (
-                <>
-                    <button onClick={goBack}>go back</button>
-                    <CustomButton to={'/'}>go home</CustomButton>
-                    <h1>{post.title}</h1>
-                    <p>{post.body}</p>
-                    <Link to={`/posts/${id}/edit`}>Edit this post</Link>
-                    
-                </>
-            )}
+            <button onClick={goBack}>go back</button>
+
+            <CustomButton to={'/'}>go home</CustomButton>
+            <Suspense fallback={<h2>Post is loading...</h2>}>
+                <Await resolve={post}>
+                    <Post/>
+                </Await>
+            </Suspense>
+            <Suspense fallback={<h2>Comments is loading...</h2>}>
+                <Await resolve={comments}>
+                    <Comments/>
+                </Await>
+            </Suspense>
+            <Link to={`/posts/${id}/edit`}>Edit this post</Link>
         </div>
 
     )
+
 }
-export { SinglePage }
+async function getSinglePostById(id) {
+
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    return res.json()
+}
+async function getCommentsByPost(id) {
+
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+    return res.json()
+}
+
+const SinglePostLoader = async ({ params }) => {
+    const id = params.id
+    return defer ({ post: await getSinglePostById(id), id, comments:  getCommentsByPost(id) })
+
+
+
+}
+export { SinglePage, SinglePostLoader }
