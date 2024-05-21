c1aeb88f6062e6c010e9d4083ee86425d33f4c53
Exelione <exelione@gmail.com>
2024-05-21 04:07:40 +0300

    add errorElement/useRouteError

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,8 @@
-
-
-import { AboutPage } from './pages/AboutPage';
-import { BlogLoader, BlogPage } from './pages/BlogPage';
-import { NotFoundPage } from './pages/NotFoundPage';
-import { HomePage } from './pages/HomePage'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import { SinglePage, SinglePostLoader } from './pages/SinglePage';
-import { CreatePost } from './pages/CreatePost';
-import EditPost from './pages/EditPost';
-import { LoginPage } from './pages/LoginPage';
-import { RequireAuth } from './hoc/RequireAuth';
+import { RouterProvider } from 'react-router-dom';
 import { AuthProvider } from './hoc/AuthProvider';
+import { router } from './router.jsx';
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path='/' element={<Layout />}>
-    <Route index element={<HomePage />} />
-    <Route path='posts' element={<BlogPage />} loader={BlogLoader}/>
-    <Route path='posts/:id' element={<SinglePage />} loader={SinglePostLoader} />
-    <Route path='posts/:id/edit' element={
-      <RequireAuth>
-        <EditPost />
-      </RequireAuth>
-    } />
-    <Route path='posts/new' element={
-      <RequireAuth>
-        <CreatePost />
-      </RequireAuth>} />
-    <Route path='about' element={<AboutPage />}>
-      <Route path="contacts" element={<p>Our contact</p>} />
-      <Route path="team" element={<h1>Our team</h1>} />
-    </Route>
-    <Route path='login' element={<LoginPage />} />
-    <Route path='*' element={<NotFoundPage />} />
-  </Route>
-))
+
 
 function App() {
 
---
+++ src/router.jsx
@@ -0,0 +1,36 @@
+import { AboutPage } from './pages/AboutPage';
+import { BlogLoader, BlogPage } from './pages/BlogPage';
+import { NotFoundPage } from './pages/NotFoundPage';
+import { HomePage } from './pages/HomePage'
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { SinglePage, SinglePostLoader } from './pages/SinglePage';
+import { CreatePost } from './pages/CreatePost';
+import EditPost from './pages/EditPost';
+import { LoginPage } from './pages/LoginPage';
+import { RequireAuth } from './hoc/RequireAuth';
+import ErrorPage from './pages/ErrorPage';
+
+
+export const router = createBrowserRouter(createRoutesFromElements(
+    <Route path='/' element={<Layout />} >
+      <Route index element={<HomePage />} />
+      <Route path='posts' element={<BlogPage />} loader={BlogLoader} errorElement={<ErrorPage/>} />
+      <Route path='posts/:id' element={<SinglePage />} loader={SinglePostLoader} />
+      <Route path='posts/:id/edit' element={
+        <RequireAuth>
+          <EditPost />
+        </RequireAuth>} />
+      <Route path='posts/new' element={
+        <RequireAuth>
+          <CreatePost />
+        </RequireAuth>
+    }/>
+      <Route path='about' element={<AboutPage />}>
+        <Route path="contacts" element={<p>Our contact</p>} />
+        <Route path="team" element={<h1>Our team</h1>} />
+      </Route>
+      <Route path='login' element={<LoginPage />} />
+      <Route path='*' element={<NotFoundPage />} />
+    </Route>
+  ))
